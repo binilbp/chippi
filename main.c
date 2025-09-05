783e280783af7eaebd5c4f1b56390122b3b72903
binil <biniloffcl@gmail.com>
2025-09-05 16:18:02 +0530

    It starts here

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void chippi(void);
+
+
+int main(void) {
+	chippi();
+	return EXIT_SUCCESS;
+}
+
+void chippi(void) {
+
+	//welcome statement with seashell unicode
+	printf("Welcom to chippi \U0001F41A\n");
+
+
+}
+
+
+
+
