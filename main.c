578b8504897cc670c5f2f2fe4693918d34afd58e
binil <biniloffcl@gmail.com>
2025-09-05 20:34:23 +0530

    Add chippi goodbye() and partial multiline read

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 void chippi(void);
 char *read_input(void);
 void chippi_hello(char *);
+void chippi_bye(char *);
 
 int main(void) {
 	chippi();
@@ -20,22 +21,25 @@ void chippi(void) {
 
 	//used to store username for chippi_hello function
 	char user_name[9] = {0};
-
+	//used to store the user input data
+	char *user_input = NULL;
 	
 	//starting the shell loop to take input commands from the user
 	while(1){
 	
 		printf("> ");
-		char *user_input = read_input();
+		user_input = read_input();
 
 		if (user_input == NULL) {
 			//if use Ctrl + D, it is registered as NULL
 			//program termination
+			chippi_bye(user_name);
 			exit(0);
 
 		} else if (strcmp(user_input, "exit") == 0) {
 			//zero means both equal, user typed exit
 			//program termination
+			chippi_bye(user_name);
 			exit(0);
 
 		} else if (strcmp(user_input, "hello") == 0) {
@@ -47,6 +51,12 @@ void chippi(void) {
 			printf("%s\n", user_input);
 		}
 	}
+
+	//cant forget to free malloc memory unless i want memory leak
+	if(user_input != NULL){
+		free(user_input);
+	}
+
 }
 
 
@@ -56,14 +66,15 @@ void chippi_hello(char *user_name) {
 	if (user_name[0] == '\0'){
 		printf("Hi there .... What should i call u?\n");
 		printf("Please enter what i should call u [max 8char]: ");
-		fgets(user_name, sizeof(user_name), stdin);	
+		fgets(user_name,9 , stdin);	
 
 		//check if '\n' is persent in username
-		if (strchr(user_name,'\n')){
+		if (strchr(user_name,'\n')!= NULL){
 			//find '\n' and replace it with '\0'
 			user_name[strcspn(user_name, "\n")] = '\0';
 		} else {
 			//add '\0' at the last of the array
+//!!BUG!! SOMEPROBLEM WITH NAME CHAR COUNT
 			user_name[9] = '\0';	
 		}
 		printf("Hi there %s\n",user_name);
@@ -73,25 +84,74 @@ void chippi_hello(char *user_name) {
 	}
 }
 
+void chippi_bye(char *user_name) {
 
-char *read_input(void) {
-	size_t capacity = 256;
-	char temp_buffer[capacity] = {};
+	if (user_name[0] == '\0'){
 
-	//sizeof(char) is 1, essentially same as providing the size directly
-	char *buffer = malloc(capacity);
-
-
-	if(fgets(buffer, sizeof(temp_buffer), stdin) == NULL) {
-
-		return NULL;
+		printf("Good Bye !\n");
 
 	} else {
+
+		printf("Good Bye %s !\n",user_name);
+	}
+}
+
+char *read_input(void) {
+	//to track the size mannually
+	size_t current_capacity = 256;
+
+	char temp_buffer[current_capacity] = {};
+
+	//sizeof(char) is 1, essentially same as providing the size directly
+	char *buffer = malloc(current_capacity);
+	//setup the raw memory for storing string
+	buffer[0] = '\0';
+
+
+	//fgets only read until the max size or '\n' or EOF
+	//for the case with multiple lines, we do while loop
+	while(1){
+
+		if(fgets(buffer, sizeof(temp_buffer), stdin) == NULL) {
+
+			//ideally input should be either '\n' or some value
+			//therefore with NULL we return NULL
+			return NULL;
+
+		}
 		
-		//find and replace the '\n' position with '\0'
-		size_t pos = strcspn(buffer, "\n");
-		buffer[pos] = '\0';	
-		return buffer;
+
+
+		//check if we need to increase memory before appending continuous data
+		if(strlen(temp_buffer) + strlen(buffer) >= current_capacity){
+			//true means we need to increase the memory size
+			char *temp = realloc(buffer, current_capacity+current_capacity);
+			if (temp == NULL){
+				perror("Allocation failed\n");
+				return NULL;
+			}
+
+			//also note the change mannually since reallocation succeeded
+			current_capacity += current_capacity;
+
+			//update the pointer old pointer with new larger sized memory pointer
+			buffer = temp;
+		}
+
+		//safely append the rest of the user data to older data
+		strcat(buffer, temp_buffer);
+
+
+		//now we check if the latest data we added have '\n'
+		//if yes it means we stop fgets loop here and return value
+		//after changing the '\n' to '\0'
+		if (strchr(buffer,'\n') != NULL){
+			//find and replace the '\n' position with '\0'
+			size_t pos = strcspn(buffer, "\n");
+			buffer[pos] = '\0';	
+			return buffer;
+		}
+		
 	}
 }
 
